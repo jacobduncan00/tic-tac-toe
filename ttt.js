9e69eb0996da11ceb2a656e3055c76817a78bb33
Jacob Duncan <jacobduncan00@yahoo.com>
2020-04-10 14:27:11 -0400

    🔨Done

--- ttt.js
+++ ttt.js
@@ -296,43 +296,47 @@ function isOver() {
   for (let col = 0; col < 3; col++) {
     let row = 0;
     if (
-      board[row][col] == board[row + 1][col] &&
-      (board[row][col] != undefined && board[row + 1][col] != undefined) ==
-        (board[row + 2][col] && board[row + 2][col] != undefined)
+      board[row][col] == board[row + 1][col] == board[row + 2][col] &&
+      (board[row][col] != undefined && board[row + 1][col] != undefined && board[row + 2][col] != undefined)
     ) {
       return true;
     }
   }
   /*checking horizontal line*/
   if (
-    board[0][0] == board[0][1] &&
-    board[0][1] == board[0][2] &&
-    board[0][0] != undefined
+    (board[0][0] == board[0][1] && board[0][1] == board[0][2]) &&
+    (board[0][0] != undefined &&
+      board[0][1] != undefined &&
+      board[0][2] != undefined)
   ) {
     return true;
   } else if (
-    board[1][0] == board[1][1] &&
-    board[1][1] == board[1][2] &&
-    board[1][0] != undefined
+    (board[1][0] == board[1][1] && board[1][1] == board[1][2]) &&
+    (board[1][0] != undefined &&
+      board[1][1] != undefined &&
+      board[1][2] != undefined)
   ) {
     return true;
   } else if (
-    board[2][0] == board[2][1] &&
-    board[2][1] == board[2][2] &&
-    board[2][0] != undefined
+    (board[2][0] == board[2][1] && board[2][1] == board[2][2]) &&
+    (board[2][0] != undefined &&
+      board[2][1] != undefined &&
+      board[2][2] != undefined)
   ) {
     return true;
     /* checking diagonal line*/
   } else if (
-    board[0][0] == board[1][1] &&
-    board[1][1] == board[2][2] &&
-    board[1][1] != undefined
+    (board[0][0] == board[1][1] && board[1][1] == board[2][2]) &&
+    (board[1][1] != undefined &&
+      board[1][1] != undefined &&
+      board[2][2] != undefined)
   ) {
     return true;
   } else if (
-    board[2][0] == board[1][1] &&
-    board[1][1] == board[0][2] &&
-    board[2][0] != undefined
+    (board[0][2] == board[1][1] && board[1][1] == board[2][0]) &&
+    (board[0][2] != undefined &&
+      board[1][1] != undefined &&
+      board[2][0] != undefined)
   ) {
     return true;
   } else {
